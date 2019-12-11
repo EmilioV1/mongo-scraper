4a66b1e3cc97dd2e4e38d5537a6583ce0d8c3c83
Valladares <vallae@amazon.com>
2019-12-11 14:49:35 -0800

    Correcting spelling to fix deploy issue.

--- controllers/headlines.js
+++ controllers/headlines.js
@@ -3,7 +3,7 @@ var scrape = require("../scripts/scrape");
 var makeDate = require("../scripts/date");
 
 //Bring in the Headline and Note mongoose models
-var Headlines = require("../models/Headline");
+var Headlines = require("../models/headlines");
 
 module.exports = {
     fetch: function (cb) {
